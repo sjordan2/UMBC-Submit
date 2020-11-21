0cf74433e1a85c12c41ee58aa8ed969c4977ee71
Sean Jordan <sjordan2@umbc.edu>
2020-11-20 22:09:03 -0500

    Update db_sql.php

--- db_sql.php
+++ db_sql.php
@@ -3,8 +3,3 @@ $sql_host = "127.0.0.1";
 $sql_username = "root";
 $sql_password = "password";
 $sql_dbname = "cmsc201_database";
-
-//$sql_host = "db1.cs.umbc.edu";
-//$sql_username = "eric8";
-//$sql_password = "sROyOUguWNTv0BPy";
-//$sql_dbname = "eric8_cmsc201_database";
