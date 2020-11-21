b11125c7e184a13140cd8d87c235816d6ca86be5
Sean Jordan <sjordan2@umbc.edu>
2020-11-21 12:13:08 -0500

    Made delete button do something

--- list_students.php
+++ list_students.php
@@ -18,14 +18,17 @@ table {border: 1px solid #000000;}
 th {border: 1px solid #000000;}
 td {border: 1px solid #000000; text-align: center}
 tr {border: 1px solid #000000;}
+h {font-size: 30px}
 </style>
 ";
+
+
 // Create connection
 $conn = new mysqli($sql_host, $sql_username, $sql_password, $sql_dbname);
 
 	// Check connection
 	if ($conn->connect_error) {
-  	die("Connection failed: " . $conn->connect_error);
+  	    die("Connection failed: " . $conn->connect_error);
 	}
     $sql_check = "SHOW TABLES LIKE 'Students';";
     $result_check = $conn->query($sql_check);
@@ -33,25 +36,40 @@ $conn = new mysqli($sql_host, $sql_username, $sql_password, $sql_dbname);
         $_SESSION["ListStudentsMessage"] = "There is no 'Students' table!";
         header('Location: main.php');
     } else {
-        echo "<table>";
-        echo "<tr><th>Last Name</th><th>First Name</th><th>Name ID</th><th>Campus ID</th><th>Role</th><th>Discussion Section</th><th>Actions</th></tr>";
         $sql_list = "SELECT lastname, firstname, umbc_name_id, umbc_id, role, section FROM Students";
         $result_list = $conn->query($sql_list);
+        echo "<h id='currNumStudents'>Number of Students: $result_list->num_rows</h>";
+        echo "<table id='student_table'>";
+        echo "<tr id='header_row'><th>Last Name</th><th>First Name</th><th>Name ID</th><th>Campus ID</th><th>Role</th><th>Discussion Section</th><th>Actions</th></tr>";
         if ($result_list->num_rows > 0) {
             // output data of each row
             $counter = 0;
             while($row = $result_list->fetch_assoc()) {
-                echo "<tr>";
+                $student_id = 'student_row_' . $row['umbc_name_id'];
+                echo "<tr id=$student_id>";
                 foreach($row as $element) {
                     echo "<td>";
                     echo $element;
                     echo "</td>";
                 }
-                echo "<td><button class='delete_button', id='delbutton' . $counter>Remove Student</button></td>";
-
+                $deleteid = "del_" . $row['umbc_name_id'];
+                echo "<td><button class='delete_button' id=$deleteid onclick='deleteStudent(this)'>Remove Student</button></td>";
                 echo "</tr>";
                 $counter++;
             }
         echo "</table>";
     }
 }
+    ?>
+<script>
+    function deleteStudent(button) {
+        let student_name_id = button.id.split("_")[1]; // Gets UMBC Name Id of Student
+        let studentRowId = "student_row_" + student_name_id;
+        let studentRow = document.getElementById(studentRowId);
+        studentRow.parentNode.removeChild(studentRow);
+        let studentTable = document.getElementById("student_table");
+        let newNumStudents = studentTable.rows.length - 1;
+        let headerNumStudents = document.getElementById("currNumStudents");
+        headerNumStudents.innerHTML = "Number of Students: " + newNumStudents;
+    }
+</script>
