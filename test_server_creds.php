0afe7b8b674a169fcdbe37bf6d610bfb25d98b42
Sean Jordan <sjordan2@umbc.edu>
2022-08-03 20:04:33 -0400

    final version

--- test_server_creds.php
+++ test_server_creds.php
@@ -1,6 +1,6 @@
 <?php
 
-$ssh_host = "ite325jpc01.cs.umbc.edu";
-$ssh_username = "sjordan2";
-$ssh_password = "sjordan24321";
+$ssh_host = "redacted";
+$ssh_username = "redacted";
+$ssh_password = "redacted";
 $docker_image_name = "umbc_submit_test";
